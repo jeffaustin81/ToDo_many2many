d7a815928f3c44d43eb02f15d508ebd072247cb0
Jeff Austin & Charles Moss <student@epicodus.com>
2015-08-24 10:50:30 -0700

    Add test for delete all, get all, and find, all pass

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -51,74 +51,93 @@
             $this->assertEquals("Feed dog", $result);
         }
 
-        // function test_getAll()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Category($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //     $test_task = new Task($description, $id);
-        //     $test_task->save();
-        //
-        //     $description2 = "Water the lawn";
-        //     $test_task2 = new Task($description2, $id);
-        //     $test_task2->save();
-        //
-        //     //Act
-        //     $result = Task::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals([$test_task, $test_task2], $result);
-        // }
+        function test_task_getId()
+        {
+            //Arrange
+            $id = 1;
+            $description = "Wash the dog";
+            $test_task = new Task($description, $id);
 
-        // function test_deleteAll()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Category($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //
-        //     $test_task = new Task($description, $id);
-        //     $test_task->save();
-        //
-        //     $description2 = "Water the lawn";
-        //     $test_task2 = new Task($description2, $id);
-        //     $test_task2->save();
-        //
-        //     //Act
-        //     Task::deleteAll();
-        //
-        //     //Assert
-        //     $result = Task::getAll();
-        //     $this->assertEquals([], $result);
-        // }
-        //
-        // function test_getId()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Category($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //
-        //     $test_task = new Task($description, $id);
-        //     $test_task->save();
-        //
-        //     //Act
-        //     $result = $test_task->getId();
-        //
-        //     //Assert
-        //     $this->assertEquals(true, is_numeric($result));
-        // }
-        //
+            //Act
+            $result = $test_task->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function test_task_save()
+        {
+            //Arrange
+            $id = 1;
+            $description = "Wash the dog";
+            $test_task = new Task($description, $id);
+
+            //Act
+            $test_task->save();
+
+            //Assert
+            $result = Task::getAll();
+            $this->assertEquals($test_task, $result[0]);
+        }
+
+        function testSaveSetsId()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
+
+            //Act
+            $test_task->save();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($test_task->getId()));
+        }
+
+
+
+
+        function test_getAll()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
+            $test_task->save();
+
+            $description2 = "Water the lawn";
+            $id2 = 2;
+            $test_task2 = new Task($description2, $id2);
+            $test_task2->save();
+
+            //Act
+            $result = Task::getAll();
+
+            //Assert
+            $this->assertEquals([$test_task, $test_task2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
+            $test_task->save();
+
+            $description2 = "Water the lawn";
+            $id2 = 2;
+            $test_task2 = new Task($description2, $id2);
+            $test_task2->save();
+
+            //Act
+            Task::deleteAll();
+
+            //Assert
+            $result = Task::getAll();
+            $this->assertEquals([], $result);
+        }
+
         // // function test_getCategoryId()
         // // {
         // //     //Arrange
@@ -139,28 +158,23 @@
         // //     $this->assertEquals(true, is_numeric($result));
         // // }
         //
-        // function test_find()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Category($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //
-        //     $test_task = new Task($description, $id);
-        //     $test_task->save();
-        //
-        //     $description2 = "Water the lawn";
-        //     $test_task2 = new Task($description2, $id);
-        //     $test_task2->save();
-        //
-        //     //Act
-        //     $result = Task::find($test_task->getId());
-        //
-        //     //Assert
-        //     $this->assertEquals($test_task, $result);
-        // }
+        function test_find()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
+            $test_task->save();
+            $id2 = 2;
+            $description2 = "Water the lawn";
+            $test_task2 = new Task($description2, $id2);
+            $test_task2->save();
+
+            //Act
+            $result = Task::find($test_task->getId());
+
+            //Assert
+            $this->assertEquals($test_task, $result);
+        }
     }
 ?>
